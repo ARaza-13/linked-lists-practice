6cf8cd42ef2a355687d6a6bf09afa2c4e7a1ce18
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-07-22 18:14:37 -0400

    Added an append method to the LinkList class that adds a new node with a specific value to the end of the linked list

---
+++ index.js
@@ -0,0 +1,9 @@
+import LinkedList from "./linked-list.js";
+
+const list = new LinkedList();
+
+list.append('Tifa');
+console.log(list);
+
+list.append('Cloud');
+console.log(list);
--- linked-list.js
+++ linked-list.js
@@ -1,7 +1,25 @@
-import Node from "./node";
+import Node from "./node.js";
 
 export default class LinkedList {
     constructor() {
         this.headNode = null;
+    }
+
+    // add a new node containing value to the end of the list 
+    append(value) {
+        const newNode = new Node(value);
+
+        // if list is empty, new node becomes the head node of the list
+        // if not empty, transverse the list to find the last node 
+        // and set its nextNode to the new node
+        if (this.headNode === null) {
+            this.headNode = newNode;
+        } else {
+            let current = this.headNode;
+            while (current.nextNode !== null) {
+                current = current.nextNode;
+            }
+            current.nextNode = newNode;
+        }
     }
 }
